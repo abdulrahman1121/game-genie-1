ff8eafd4edfdaf73429ab339b1c375c2129c5f51
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2025-07-31 14:24:54 -0700

    Apply flip animations in rewards page when the middle button is clicked, It shows the unscramble component

---
+++ frontend/src/components/Unscramble.jsx
@@ -0,0 +1,13 @@
+import './Unscramble.css'
+
+
+function Unscramble () {
+    return (
+    <div className='Unscramble-comp'>
+        <img src="/unscramble-image.png" alt="unscramble" className='unscramble-img'/>
+    </div>
+)
+
+}
+
+export default Unscramble;
--- frontend/src/pages/RewardsPage.jsx
+++ frontend/src/pages/RewardsPage.jsx
@@ -1,12 +1,16 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import GoBackImage from '../components/GoBackImage.jsx';
 import SettingsImage from '../components/SettingsImage.jsx';
+import Unscramble from "../components/Unscramble.jsx";
 import './RewardsPage.css';
+import { useState } from "react";
 
 function RewardsPage() {
   const navigate = useNavigate();
   const { state } = useLocation();
   const { guessCount = 0, points = 0 } = state || {};
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [showUnscramble, setShowUnscramble] = useState(false);
 
   return (
     <div className="rewards-page">
@@ -18,20 +22,34 @@ function RewardsPage() {
         <div className="genie-container2">
           <img src="/genie.png" alt="genie-image" className='genie-image2'/>
         </div>
-        <div className="rewards-component">
-          <img src="/rewards.png" alt="rewards" className="rewards-image" />
-          <p className="rewards-tries"> {guessCount} </p>
-          <p className="rewards-points">+ {points}</p>
+        <div className='rewards-component'>
+          {showUnscramble ? (
+            <Unscramble />
+          ): (
+            <>
+              <img src="/rewards.png" alt="rewards" className={`rewards-image ${isFlipped ? 'flip' : ''}`}/>
+              <p className={`rewards-tries ${isFlipped ? 'flip' : ''}`}> {guessCount} </p>
+              <p className={`rewards-points ${isFlipped ? 'flip' : ''}`}>+ {points}</p>
+              <button className={`home-button ${isFlipped ? 'flip' : ''}`} onClick={() => navigate('/')}>
+                <img src="/home-image.png" alt="home" className="home-image"/>
+              </button>
+              <button className={`reset-button ${isFlipped ? 'flip' : ''}`} onClick={() => navigate('/game')}>
+                <img src="/reset.png" alt="reset" className="reset-image"/>
+              </button>
+              <button className={`flip-button ${isFlipped ? 'flip' : ''}`} 
+                onClick={() => {
+                  setIsFlipped(true);
+                  setTimeout(() => {
+                    setShowUnscramble(true);
+                    setIsFlipped(false);
+                  }, 600) // match animation duration
+                }}>
+                <img src="flip-image.png" alt="flip" className="flip-image"/>
+              </button>
+            </>
+          )}
+          
         </div>
-        <button className="home-button" onClick={() => navigate('/')}>
-          <img src="/home-image.png" alt="home" className="home-image"/>
-        </button>
-        <button className="reset-button" onClick={() => navigate('/game')}>
-          <img src="/reset.png" alt="reset" className="reset-image"/>
-        </button>
-        <button className="flip-button">
-          <img src="flip-image.png" alt="flip" className="flip-image"/>
-        </button>
       </div>
     </div>
   );
