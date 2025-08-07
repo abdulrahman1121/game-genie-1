1be1007e7a8a9140a79ccb023510b0076807151e
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2025-08-06 17:25:43 -0700

    Modified main.jsx to use HashRouter to resolve errors when deploying, added a meta tag to index.html to relax CSP

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { HashRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
