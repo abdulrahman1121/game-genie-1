57aa1dc1e3f5e8919a6321a3ed389e70528ae05d
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2025-08-11 13:07:08 -0700

    Removed uneeded import of firebase json file

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,5 @@
 // In backend/firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
 require('dotenv').config();
 
 admin.initializeApp({
