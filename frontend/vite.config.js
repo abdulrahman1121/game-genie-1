853a2c9d51cc6741a3d9acebaa64769b3b71c1dd
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2025-08-18 16:33:02 -0700

    Remove localhost from vite.config.js and use render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000' // Match backend port
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': 'http://localhost:3000' // Match backend port
+  //   }
+  // },
   resolve: {
     alias: {
       'firebase/firestore': 'firebase/firestore'
