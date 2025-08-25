3ada1eb35b65950dfb27c3975a6224e7654e41bd
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2025-08-25 12:25:56 -0700

    slight changes to styling of welcome modal and classname changes

--- frontend/src/components/WelcomeModal.jsx
+++ frontend/src/components/WelcomeModal.jsx
@@ -6,7 +6,7 @@ function WelcomeModal({ onClose }) {
         <div className='modal-content'>
         <img src={`${import.meta.env.BASE_URL}guide.png`} alt="guide-image" className='guide-img'/>
         <button className="start-button" onClick={onClose}>
-          <img src={`${import.meta.env.BASE_URL}start-button.png`} alt="" className=''/>
+          <img src={`${import.meta.env.BASE_URL}start-button.png`} alt="start-button" className='start-btn'/>
         </button>
         </div>
     </div>
