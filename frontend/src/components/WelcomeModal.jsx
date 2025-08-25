04d7b7aeb3a3854f975f74e7ef2cbf9fa1285854
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2025-08-25 11:45:49 -0700

    Added the new tutorial guide to gamepage

--- frontend/src/components/WelcomeModal.jsx
+++ frontend/src/components/WelcomeModal.jsx
@@ -3,26 +3,12 @@ import './WelcomeModal.css';
 function WelcomeModal({ onClose }) {
   return (
     <div className="modal-overlay">
-      <div className="modal-text-box">
-        <p className="welcome-message">Welcome to Game Genie!</p>
-        <div className="tutorial-tiles">
-          <div className="tutorial-tile">
-            <img src={`${import.meta.env.BASE_URL}blue-t.png`} alt="Correct Tile" className="tile-image" />
-            <p>Correct letter and position</p>
-          </div>
-          <div className="tutorial-tile">
-            <img src={`${import.meta.env.BASE_URL}yellow-t.png`} alt="Present Tile" className="tile-image" />
-            <p>Correct letter, wrong position</p>
-          </div>
-          <div className="tutorial-tile">
-            <img src={`${import.meta.env.BASE_URL}grey-t.png`} alt="Incorrect Tile" className="tile-image" />
-            <p>Letter not in word</p>
-          </div>
-        </div>
+        <div className='modal-content'>
+        <img src={`${import.meta.env.BASE_URL}guide.png`} alt="guide-image" className='guide-img'/>
         <button className="start-button" onClick={onClose}>
-          Start Game
+          <img src={`${import.meta.env.BASE_URL}start-button.png`} alt="" className=''/>
         </button>
-      </div>
+        </div>
     </div>
   );
 }
