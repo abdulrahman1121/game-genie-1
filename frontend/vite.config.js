29727a4e073991404e393c93aa6463478854facc
abdulrahman1121 <abdulelmi98@gmail.com>
2026-06-13 16:44:24 -0700

    Changed domain to gogamegenie

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/game-genie-1/' : '/', // important for GH Pages
+  base: '/', 
   
 }));
 
